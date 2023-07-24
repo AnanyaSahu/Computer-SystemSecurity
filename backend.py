5f5fb5d055b93084e60a41fe80fc510a48be4ca6
ananya <ananya.sahu0894@gmail.com>
2023-07-24 18:11:32 +0100

    key generation

--- backend.py
+++ backend.py
@@ -1,17 +1,29 @@
 from database import databaseConnection
 
+# import required module
+from cryptography.fernet import Fernet
+global fernetObj;
 
 class backend:
   def __init__(self):
-    pass
+    # key generation
+    key = "b'Nse-YkE5Y_Me_nTciyCd9Elr0F3nlyj0uhkRGcsegD8='"
+    self.fernetObj = Fernet(key)
+
+
       # this method will get the message
   def getMessagesForUser(self,userId):
+    list = []
     d = databaseConnection()
     cursor = d.openDbConnection()
     query = "select notificationId,messageText,senderId from [dbo].[notifications] where [isOpened] = 0 and [recieverId] = "+str(userId)+";" 
     record = cursor.execute(query).fetchall()
     r= [tuple(row) for row in record]
-    return {'rows': r}
+    for tup in r:
+      newTuple = (tup[0], self.fernetObj.decrypt(tup[1]), tup[2])
+      list.append(newTuple)
+    
+    return {'rows': list}
   
 
         # this method will create the message
@@ -19,7 +31,8 @@ class backend:
     d = databaseConnection()
     cursor = d.openDbConnection()
     query = "insert into [dbo].[notifications]  values (?,?,?,?)" 
-    c= cursor.execute(query, str( userId),  str(params['recieverId']), str(params['message']), 1)
+    encryptedMsg = self.fernetObj.encrypt(params['message'])
+    c= cursor.execute(query, str( userId),  str(params['recieverId']), encryptedMsg , 1)
     record = c.fetchall()
     r= [tuple(row) for row in record]
     return {'rows': r}
