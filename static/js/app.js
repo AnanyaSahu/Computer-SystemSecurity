73d51a6bb65199975b050c748a1550df728dce15
ananya <ananya.sahu0894@gmail.com>
2023-07-26 20:01:46 +0100

    xheck

--- static/js/app.js
+++ static/js/app.js
@@ -149,7 +149,7 @@ async function checkUserAvailabliity(){
         method: 'GET'
     }).then(response => response.json())
     .then((data) => {
-        console.log(data.rows)
+        console.log(data)
         if(data.rows.length == 0) {
             
             messageArea.style.display = 'none'
